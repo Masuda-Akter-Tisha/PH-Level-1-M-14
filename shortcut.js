a0833cc8a66965f82398bc99f4fea6749bbba2e3
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-01-28 20:36:30 +0600

    added shortcut.js

---
+++ shortcut.js
@@ -0,0 +1,22 @@
+const isLeader = true;
+//if(isLeader === true)
+if(isLeader){
+    console.log('leave the chair,the leader is coming');
+}
+else{
+    console.log('you will not get up from the chair')
+}
+
+let isPassed = false;
+//if(isPassed === false)
+if(!isPassed){
+    console.log('get her married')
+}
+else{
+    console.log('tare porabu');
+}
+console.log(!false);
+console.log(!true);
+
+console.log(!!false);
+console.log(!!true);
